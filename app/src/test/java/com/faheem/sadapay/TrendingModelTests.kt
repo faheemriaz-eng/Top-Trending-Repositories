d6262f6a427e1e88c4dcd076d129324970dde223
Faheem Riaz <faheem@digitify.com>
2022-07-09 17:04:40 +0500

    Add test to verify model parsing

---
+++ app/src/test/java/com/faheem/sadapay/TrendingModelTests.kt
@@ -0,0 +1,24 @@
+package com.faheem.sadapay
+
+import com.faheem.sadapay.model.TrendingRepositories
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.junit.Assert
+import org.junit.Test
+
+class TrendingModelTests {
+
+    @Test
+    fun `test json response maps to model`() {
+        val trendingRepositories = readJsonFile("MockTrendingRepositories.json")
+        Assert.assertEquals(30, trendingRepositories.items?.count())
+        Assert.assertEquals("golang/go", trendingRepositories.items?.first()?.fullName)
+        Assert.assertEquals("ethereum/solidity", trendingRepositories.items?.last()?.fullName)
+    }
+
+    private fun readJsonFile(fileName: String): TrendingRepositories {
+        val gson = GsonBuilder().create()
+        val itemType = object : TypeToken<TrendingRepositories>() {}.type
+        return gson.fromJson(ReadAssetFile.readFileFromTestResources(fileName), itemType)
+    }
+}
