bc920cfe102503e821c241c0423bd9f05383633b
Faheem Riaz <faheem@digitify.com>
2022-07-09 16:47:03 +0500

    Add trending repository model

---
+++ app/src/main/java/com/faheem/sadapay/model/TrendingRepositories.kt
@@ -0,0 +1,33 @@
+package com.faheem.sadapay.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class TrendingRepositories(
+    @SerializedName("incomplete_results")
+    val incompleteResults: Boolean? = null,
+    @SerializedName("items")
+    val items: List<Item>? = null,
+    @SerializedName("total_count")
+    val totalCount: Int? = null
+)
+
+data class Item(
+    @SerializedName("full_name")
+    val fullName: String? = null,
+    @SerializedName("description")
+    val description: String? = null,
+    @SerializedName("language")
+    val language: String? = null,
+    @SerializedName("stargazers_count")
+    val stargazersCount: Int? = null,
+    @SerializedName("owner")
+    val owner: Owner? = null
+)
+
+data class Owner(
+    @SerializedName("login")
+    val login: String? = null,
+    @SerializedName("avatar_url")
+    val avatarUrl: String? = null
+)
